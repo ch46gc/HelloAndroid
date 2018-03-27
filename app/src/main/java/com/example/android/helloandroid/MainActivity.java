d41996dd2ec66d80b91abec36cf13ad348ad7615
ch46gc <chriseyhillgen@gmail.com>
2018-03-27 11:51:42 -0400

    Add dimens.xml

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 import android.view.WindowManager;
 
 import com.example.android.helloandroid.R;
